76ada75ba18ebf01e9d8ec9336edbf6f3cbbff51
mdlconsulting <mdlekgau@gmail.com>
2024-11-13 15:10:23 +0200

    fixed file paths

--- src/prediction.js
+++ src/prediction.js
@@ -1,8 +1,12 @@
 import * as tf from '@tensorflow/tfjs-node';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 async function loadModel() {
-  return await tf.loadLayersModel('file://models/trained_model/model.json');
+  return await tf.loadLayersModel(`file://${path.join(__dirname, '../models/trained_model/model.json')}`);
 }
 
 async function makePrediction({ latitude, longitude, date }) {
