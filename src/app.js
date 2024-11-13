2d709a85d733864b80a500f6c1f6dd79a93cbb0b
mdlconsulting <mdlekgau@gmail.com>
2024-11-13 15:24:04 +0200

    added weights.bin file

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { makePrediction } from './src/prediction.js';
+import { makePrediction } from './prediction.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
